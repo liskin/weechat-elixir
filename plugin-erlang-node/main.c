99cea6f09dec403cb2709da934a3a44daf83da59
Tomas Janousek <tomi@nomi.cz>
2020-02-29 17:08:57 +0000

    plugin-erlang-node: Use glib for cleanups and lists

--- plugin-erlang-node/main.c
+++ plugin-erlang-node/main.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <time.h>
 
+#include <glib.h>
+
 #include <ei.h>
 #include <weechat-plugin.h>
 
@@ -11,21 +13,25 @@
 #define TIMEOUT_NORMAL 1000
 #define LISTEN_BACKLOG 10
 
+G_DEFINE_AUTO_CLEANUP_CLEAR_FUNC(ei_x_buff, ei_x_free);
+
 static struct t_weechat_plugin *weechat_plugin = NULL;
 
-struct client {
-	struct client *next;
+typedef struct client {
 	int fd;
 	struct t_hook *fd_hook;
 	char nodename[MAXNODELEN + 1];
-};
+} client_t;
+
+static void client_free(client_t *client);
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(client_t, client_free);
 
 static struct global {
 	ei_cnode ec;
 	int server_fd;
 	struct t_hook *server_fd_hook;
 	int publish_fd;
-	struct client *clients;
+	GList *clients;
 } global = {
 	.server_fd = -1,
 	.server_fd_hook = NULL,
@@ -49,61 +55,36 @@ static void safe_unhook(struct t_hook **hook) {
 
 static int client_sock_cb(const void *pointer, void *data, int client_fd);
 
-static struct client *client_add(int fd, const char *nodename) {
-	struct client *clients_head = NULL;
+static int client_add(int fd, const char *nodename) {
+	g_autoptr(client_t) client = g_new(client_t, 1);
 
-	clients_head = malloc(sizeof(*clients_head));
-	if (clients_head == NULL)
-		goto error;
-
-	clients_head->fd = fd;
-	clients_head->fd_hook = weechat_hook_fd(fd,
+	client->fd = fd;
+	client->fd_hook = weechat_hook_fd(fd,
 		1 /* flag_read */, 0 /* flag_write */, 0 /* flag_exception */,
-		client_sock_cb, clients_head, NULL);
-	if (clients_head->fd_hook == NULL) {
+		client_sock_cb, client, NULL);
+	if (client->fd_hook == NULL) {
 		weechat_printf(NULL, "erl/client_add: error (weechat_hook_fd)");
-		goto error;
+		return WEECHAT_RC_ERROR;
 	}
 
-	strncpy(clients_head->nodename, nodename, MAXNODELEN);
-	clients_head->nodename[MAXNODELEN] = 0;
+	strncpy(client->nodename, nodename, MAXNODELEN);
+	client->nodename[MAXNODELEN] = 0;
 
-	clients_head->next = global.clients;
-	global.clients = clients_head;
+	global.clients = g_list_prepend(global.clients, g_steal_pointer(&client));
 
-	return clients_head;
-
-error:
-	safe_unhook(clients_head ? &clients_head->fd_hook : NULL);
-	safe_close_fd(&fd);
-	return NULL;
+	return WEECHAT_RC_OK;
 }
 
-static void client_remove(struct client **clients_head) {
-	struct client *tmp = *clients_head;
-
+static void client_free(client_t *client) {
 	// TODO: https://github.com/weechat/weechat/blob/feb6258910d7fe907fddeea32e57f786079d82ec/src/plugins/plugin-script.c#L1046 (use nodename)
-	safe_unhook(&tmp->fd_hook);
-	safe_close_fd(&tmp->fd);
 
-	*clients_head = tmp->next;
-	free(tmp);
+	safe_unhook(&client->fd_hook);
+	safe_close_fd(&client->fd);
+
+	g_free(client);
 }
 
-static struct client **client_find(int fd) {
-	struct client **clients_head = &global.clients;
-
-	while (*clients_head) {
-		if ((*clients_head)->fd == fd)
-			return clients_head;
-
-		clients_head = &(*clients_head)->next;
-	}
-
-	return NULL;
-}
-
-static int client_handle_reg_send(struct client *client,
+static int client_handle_reg_send(client_t *client,
 		erlang_msg *msg, ei_x_buff *x, ei_x_buff *x_reply) {
 	int index = 0;
 
@@ -134,42 +115,37 @@ error:
 	return ei_x_encode_atom(x_reply, "error");
 }
 
-static int client_handle_msg(struct client *client, erlang_msg *msg, ei_x_buff *x) {
-	ei_x_buff x_reply;
+static int client_handle_msg(client_t *client, erlang_msg *msg, ei_x_buff *x) {
+	g_auto(ei_x_buff) x_reply;
 
 	if (ei_x_new_with_version(&x_reply))
-		goto error;
+		return WEECHAT_RC_ERROR;
 
 	switch (msg->msgtype) {
 		case ERL_REG_SEND:
 			if (client_handle_reg_send(client, msg, x, &x_reply))
-				goto error;
+				return WEECHAT_RC_ERROR;
 			if (ei_send_tmo(client->fd, &msg->from, x_reply.buff, x_reply.index, TIMEOUT_NORMAL) == ERL_ERROR)
-				goto error;
+				return WEECHAT_RC_ERROR;
 			break;
 		case ERL_LINK: /* we don't need to react to this, BEAM will handle it */
 			break;
 		case ERL_EXIT: /* someone linked to us and died, close connection */
-			goto error;
+			return WEECHAT_RC_ERROR;
 		case ERL_EXIT2: /* someone killed us, close connection */
-			goto error;
+			return WEECHAT_RC_ERROR;
 		default:
 			weechat_printf(NULL, "erl/client_handle_msg: unexpected msgtype %ld", msg->msgtype);
 	}
 
-	ei_x_free(&x_reply);
-	return 0;
-
-error:
-	ei_x_free(&x_reply);
-	return ERL_ERROR;
+	return WEECHAT_RC_OK;
 }
 
 static int client_sock_cb(const void *pointer, void *data, int client_fd) {
-	struct client *client = (struct client *) pointer;
+	client_t *client = (client_t *) pointer;
 
 	erlang_msg msg;
-	ei_x_buff x;
+	g_auto(ei_x_buff) x;
 
 	if (ei_x_new(&x))
 		goto error;
@@ -180,19 +156,19 @@ static int client_sock_cb(const void *pointer, void *data, int client_fd) {
 		case ERL_TICK:
 			break;
 		case ERL_MSG:
-			if (client_handle_msg(client, &msg, &x) == 0)
-				break;
+			if (client_handle_msg(client, &msg, &x) != WEECHAT_RC_OK)
+				goto error;
+			break;
 		case ERL_ERROR:
 			goto error;
 	}
 
-	ei_x_free(&x);
 	return WEECHAT_RC_OK;
 
 error:
 	weechat_printf(NULL, "erl/client_sock_cb: disconnecting client %s", client->nodename);
-	ei_x_free(&x);
-	client_remove(client_find(client_fd));
+	global.clients = g_list_remove(global.clients, client);
+	client_free(client);
 	return WEECHAT_RC_ERROR;
 }
 
@@ -210,27 +186,25 @@ static int server_sock_cb(const void *pointer, void *data, int server_fd) {
 		}
 	}
 
-	if (client_add(fd, conn.nodename) == NULL)
+	if (client_add(fd, conn.nodename) != WEECHAT_RC_OK)
 		return WEECHAT_RC_ERROR;
 
 	weechat_printf(NULL, "erl/server_sock_cb: connected client %s", conn.nodename);
 	return WEECHAT_RC_OK;
 }
 
-#pragma GCC visibility push(default)
+void server_cleanup() {
+	g_list_free_full(g_steal_pointer(&global.clients), (GDestroyNotify) client_free);
 
-WEECHAT_PLUGIN_NAME("erl");
-WEECHAT_PLUGIN_DESCRIPTION("Erlang C node plugin for WeeChat");
-WEECHAT_PLUGIN_AUTHOR("Tomas Janousek <tomi@nomi.cz>");
-WEECHAT_PLUGIN_VERSION(PLUGIN_ERLANG_NODE_VERSION);
-WEECHAT_PLUGIN_LICENSE("GPL3");
+	safe_close_fd(&global.publish_fd);
 
-int weechat_plugin_init(struct t_weechat_plugin *plugin, int argc, char *argv[])
-{
+	safe_unhook(&global.server_fd_hook);
+	safe_close_fd(&global.server_fd);
+}
+
+int server_init() {
 	struct in_addr loopback_addr = { .s_addr = htonl(INADDR_LOOPBACK) };
 	int port = 0;
-
-	weechat_plugin = plugin;
 
 	if (ei_init()) {
 		weechat_printf(NULL, "erl/weechat_plugin_init: error (ei_init)");
@@ -274,21 +248,29 @@ int weechat_plugin_init(struct t_weechat_plugin *plugin, int argc, char *argv[])
 	return WEECHAT_RC_OK;
 
 error:
-	safe_close_fd(&global.publish_fd);
-	safe_close_fd(&global.server_fd);
+	server_cleanup();
 
 	return WEECHAT_RC_ERROR;
 }
 
+#pragma GCC visibility push(default)
+
+WEECHAT_PLUGIN_NAME("erl");
+WEECHAT_PLUGIN_DESCRIPTION("Erlang C node plugin for WeeChat");
+WEECHAT_PLUGIN_AUTHOR("Tomas Janousek <tomi@nomi.cz>");
+WEECHAT_PLUGIN_VERSION(PLUGIN_ERLANG_NODE_VERSION);
+WEECHAT_PLUGIN_LICENSE("GPL3");
+
+int weechat_plugin_init(struct t_weechat_plugin *plugin, int argc, char *argv[])
+{
+	weechat_plugin = plugin;
+
+	return server_init();
+}
+
 int weechat_plugin_end(struct t_weechat_plugin *plugin)
 {
-	while (global.clients)
-		client_remove(&global.clients);
-
-	safe_close_fd(&global.publish_fd);
-
-	safe_unhook(&global.server_fd_hook);
-	safe_close_fd(&global.server_fd);
+	server_cleanup();
 
 	return WEECHAT_RC_OK;
 }
